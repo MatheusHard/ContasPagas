da29a87cdb8ac19638ca92eaa3e5331030e86a3e
MatheusHard <matheushard2013@gmail.com>
2021-05-07 15:50:06 -0300

    Designer Card Principal ok

--- contas_pagas_2021/lib/home/widgets/cards/card_inicio.dart
+++ contas_pagas_2021/lib/home/widgets/cards/card_inicio.dart
@@ -1,24 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:pagamento_de_contas/iu/cadastrar_conta.dart';
+import 'package:pagamento_de_contas/iu/listar_contas.dart';
 import 'package:pagamento_de_contas/utils/core/app_colors.dart';
 import 'package:pagamento_de_contas/utils/core/app_text_styles.dart';
 
 class Card_Principal extends StatefulWidget {
 
-  const Card_Principal({Key key}) : super(key: key);
+  IconData icone;
+  String texto;
+  Color cor;
+  int index;
+
+   Card_Principal({Key key, @required this.icone, @required this.texto,
+                            @required this.cor, @required this.index}) : super(key: key);
 
   @override
-  _Card_PrincipalState createState() => _Card_PrincipalState();
+  _Card_PrincipalState createState() => _Card_PrincipalState(icone, texto, cor, index);
 }
 
 class _Card_PrincipalState extends State<Card_Principal> {
+
+  IconData _icon;
+  String _texto;
+  Color _cor;
+  int _index;
+
+  _Card_PrincipalState(IconData icon, String texto, Color cor, int index){
+    this._icon = icon;
+    this._texto = texto;
+    this._cor = cor;
+    this._index = index;
+  }
+
+
   @override
   Widget build(BuildContext context) {
+
+
+    final pages = [
+      Cadastrar_Conta(conta: null,),
+      Listar_Contas()
+    ];
+
     return
 
 
             GestureDetector(
-              onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => Cadastrar_Conta(conta: null,))), // handle your image tap here
 
               child: Container(
                 decoration: BoxDecoration(
@@ -32,8 +59,8 @@ class _Card_PrincipalState extends State<Card_Principal> {
 
                     Center(child:
                     Icon(
-                      Icons.attach_money_sharp,
-                      color: AppColors.green,
+                      _icon,
+                      color: this._cor,
                       size: 50,),),
 
                 Align(
@@ -41,11 +68,15 @@ class _Card_PrincipalState extends State<Card_Principal> {
                     child:
                       Padding(
                         padding: const EdgeInsets.only(bottom: 20, top: 50),
-                        child: Text("Cadastro", style: AppTextStyles.bodyBold,),
+                        child: Text(this._texto, style: AppTextStyles.bodyBold,),
                     ))
                   ],
                 ),
               ),
+
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => pages[_index])), //
             );
 
 
--- contas_pagas_2021/lib/iu/principal.dart
+++ contas_pagas_2021/lib/iu/principal.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pagamento_de_contas/home/widgets/cards/card_inicio.dart';
+import 'package:pagamento_de_contas/utils/core/app_colors.dart';
 import 'package:pagamento_de_contas/utils/core/app_images.dart';
 import 'package:pagamento_de_contas/home/widgets/appbar/app_bar_widget.dart';
 import 'package:pagamento_de_contas/iu/cadastrar_conta.dart';
@@ -54,8 +55,10 @@ Padding(
 
                       ),
 
-                      Card_Principal()
-                    ],
+                      Card_Principal(icone: Icons.attach_money_sharp, texto: "Cadastrar", cor: AppColors.green, index: 0,),
+                      Card_Principal(icone: Icons.text_snippet_sharp, texto: "Historico", cor: AppColors.lightRed, index: 1,)
+
+              ],
                 ),
               )
          );
